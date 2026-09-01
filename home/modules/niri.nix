448191c4dc1cb9698eb964ff5985efe8d813969a
Sultan Majed <sultan.m.alsalahi@gmail.com>
2026-09-01 10:18:34 +0300

    fix: niri monitor setting

--- home/modules/niri.nix
+++ home/modules/niri.nix
@@ -8,6 +8,7 @@
 
   xdg.configFile."niri/config.kdl".source = ../../assets/niri/config.kdl;
   xdg.configFile."niri/noctalia.kdl".source = ../../assets/niri/noctalia.kdl;
+  # xdg.configFile."niri/monitor.kdl".source = ../../assets/niri/monitor.kdl;
   xdg.configFile."niri/monitor-mirroring.sh".source = ../../assets/niri/monitor-mirroring.sh;
 
   home.file.".local/bin/monitor-mirroring.sh" = {
