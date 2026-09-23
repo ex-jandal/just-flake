c6f6d887c783ce832405fd8178766dbf8b80cd1d
Sultan Majed <sultan.m.alsalahi@gmail.com>
2026-09-23 22:01:09 +0300

    fix yazi deprcated config

--- home/modules/yazi.nix
+++ home/modules/yazi.nix
@@ -2,17 +2,41 @@
   pkgs,
   ...
 }:
+let
+  yaziPlugins = pkgs.yaziPlugins;
+  enablePlugins =
+    names:
+    builtins.listToAttrs (
+      map (name: {
+        inherit name;
+        value = {
+          package = yaziPlugins.${name};
+        };
+      }) names
+    );
+in
 {
   programs.yazi = {
     enable = true;
     shellWrapperName = "y";
+    plugins = enablePlugins [
+      "git"
+      "lsar"
+      "full-border"
+      "mount"
+      "smart-enter"
+      "smart-filter"
+      "toggle-pane"
+      "vcs-files"
+      "chmod"
+      "mime-ext"
+    ];
   };
 
   home.file.".config/yazi/yazi.toml".source = ../../assets/yazi/yazi.toml;
   home.file.".config/yazi/init.lua".source = ../../assets/yazi/init.lua;
   home.file.".config/yazi/keymap.toml".source = ../../assets/yazi/keymap.toml;
   home.file.".config/yazi/theme.toml".source = ../../assets/yazi/theme.toml;
-  home.file.".config/yazi/package.toml".source = ../../assets/yazi/package.toml;
 
   home.packages = with pkgs; [
     ueberzugpp
